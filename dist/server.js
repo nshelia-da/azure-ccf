4706216990230c1ce8337c69858f07e87cf4a166
nshelia <nshprimary@gmail.com>
2023-05-18 05:29:37 +0400

    read blobs

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,60 @@
 "use strict";
-console.log('Hello world!');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+console.log('Hello world!', process.env.AZURE_STORAGE_CONNECTION_STRING);
+const { BlobServiceClient } = require('@azure/storage-blob');
+// Function to download and console.log the file content
+function downloadBlob(containerName, blobName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+        try {
+            const response = yield blockBlobClient.download(0);
+            const content = yield streamToString(response.readableStreamBody);
+            console.log(`Downloaded '${blobName}' blob content:`, content);
+        }
+        catch (error) {
+            console.error(`Error downloading '${blobName}' blob:`, error);
+        }
+    });
+}
+// Helper function to read a stream and convert it to a string
+function streamToString(readableStream) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            const chunks = [];
+            readableStream.on('data', (data) => {
+                chunks.push(data.toString());
+            });
+            readableStream.on('end', () => {
+                resolve(chunks.join(''));
+            });
+            readableStream.on('error', reject);
+        });
+    });
+}
+// Main function
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const containerName = 'example-read-container';
+        const blobName = 'sample-file.csv';
+        yield downloadBlob(containerName, blobName);
+    });
+}
+main().catch((error) => {
+    console.error('Error running the sample:', error.message);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,49 @@
-console.log('Hello world!');
+import dotenv from 'dotenv';
+import { Readable } from 'stream'
+
+dotenv.config();
+
+const { BlobServiceClient } = require('@azure/storage-blob');
+
+// Function to download and console.log the file content
+async function downloadBlob(containerName: string, blobName: string) {
+  const blobServiceClient = BlobServiceClient.fromConnectionString(
+    process.env.AZURE_STORAGE_CONNECTION_STRING
+  );
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  try {
+    const response = await blockBlobClient.download(0);
+    const content = await streamToString(response.readableStreamBody);
+    console.log(`Downloaded '${blobName}' blob content:`, content);
+  } catch (error) {
+    console.error(`Error downloading '${blobName}' blob:`, error);
+  }
+}
+
+// Helper function to read a stream and convert it to a string
+async function streamToString(readableStream: Readable): Promise<string> {
+    return new Promise((resolve: (value: string) => void, reject: (reason?: any) => void) => {
+      const chunks: string[] = [];
+      readableStream.on('data', (data: Buffer) => {
+        chunks.push(data.toString());
+      });
+      readableStream.on('end', () => {
+        resolve(chunks.join(''));
+      });
+      readableStream.on('error', reject);
+    });
+  }
+
+// Main function
+async function main() {
+  const containerName = 'example-read-container';
+  const blobName = 'sample-file.csv';
+
+  await downloadBlob(containerName, blobName);
+}
+
+main().catch((error) => {
+  console.error('Error running the sample:', error.message);
+});
