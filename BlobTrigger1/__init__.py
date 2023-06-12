6ff07de2a0f43de14541e558beaa01e6ab0bfd92
Nika Shelia <nika.shelia@dataart.com>
2023-06-13 02:31:41 +0400

    put secret keys in env

--- BlobTrigger1/__init__.py
+++ BlobTrigger1/__init__.py
@@ -3,16 +3,19 @@ import azure.functions as func
 import json
 from azure.storage.blob import BlobServiceClient
 import logging
+import os
 
 
 def main(myTimer: func.TimerRequest, outputQueueItem: func.Out[str]) -> None:
     logging.info('Python timer trigger function started.')
     # Create a blob service client
-    blob_service_client = BlobServiceClient.from_connection_string(
-        "DefaultEndpointsProtocol=https;AccountName=samplecloudfoodprint;AccountKey=KA/s/UYp8ejBYtwzfAVg3/nCF8x5mkRj4L3x/3Qgs7BeIcJ9zqM/w9Ma4zo3G1TTARh3YdsCpaN3+ASt92IGgw==;EndpointSuffix=core.windows.net")
+    blob_storage_connection_string = os.environ["samplecloudfoodprint_STORAGE"]
+    queue_name = os.environ["queue_name"]
+
+    blob_service_client = BlobServiceClient.from_connection_string(blob_storage_connection_string)
 
     # Get a reference to the container
-    container_client = blob_service_client.get_container_client("example-read-container")
+    container_client = blob_service_client.get_container_client(queue_name)
 
     # List all the blobs in the container
     blob_list = container_client.list_blobs()
