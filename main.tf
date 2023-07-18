6fe9b0c236efe26be1ce77d3eb4610fd43b62b7a
Nika Shelia <nshprimary@gmail.com>
2023-07-18 15:38:51 +0400

    update tf

--- main.tf
+++ main.tf
@@ -4,186 +4,26 @@ provider "azurerm" {
 
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "main" {
+  name     = "ccf-resources"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-}
-
-resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_subnet.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_network_security_rule" "example" {
-  name                        = "SSH"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "192.168.0.0/24"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.example.name
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "samplecloudfoodprint"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "main" {
+  name                     = "ccf-storage-account"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "example" {
-  name                  = "example-storage-container"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
-}
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "osdisk-example"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "example-vm"
-    admin_username = "adminuser"
-    admin_password = "P@ssw0rd123!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  boot_diagnostics {
-    enabled     = true
-    storage_uri = azurerm_storage_account.example.primary_blob_endpoint
-  }
-}
-
-resource "azurerm_storage_container" "read_container" {
-  name                  = "example-read-container"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "write_container" {
-  name                  = "example-write-container"
-  storage_account_name  = azurerm_storage_account.example.name
+  name                  = "result-container"
+  storage_account_name  = azurerm_storage_account.main.name
   container_access_type = "private"
 }
 
-variable "hello_py_path" {
-  type    = string
-  default = "./hello.py"
-}
-resource "azurerm_resource_group" "azure-functions-smth" {
-  name     = "azure-functions-example-rg"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "azurefun_storage_account" {
-  name                     = "functionsappexamlpesa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "azure-functions-example-sp"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      kind
-    ]
-  }
-}
-
-resource "azurerm_function_app" "example" {
-  name                       = "cloudfootprint-azure-function"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  os_type                    = "linux"
-  version                    = "~4"
-
-
-  site_config {
-    linux_fx_version = "python|3.9"
-  }
-}
-
-resource "azurerm_storage_queue" "example" {
-  name                 = "example-queue"
-  storage_account_name = azurerm_storage_account.example.name
+resource "azurerm_storage_queue" "queue_details" {
+  name                 = "processor-queue"
+  storage_account_name = azurerm_storage_account.main.name
 }
